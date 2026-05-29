a93d6455db04c14780feee3c53d1c59ba1fe7d88
sunghyochang <xx1733@naver.com>
2026-05-29 14:56:05 +0900

    Feat: 여러문서 로드처리

--- app.py
+++ app.py
@@ -3,36 +3,37 @@ import requests as req
 
 # 환경변수, 상수
 API_URL = 'http://localhost:8000/chat'
-
-# 프론트 외관
+# 프런트 외관
 st.set_page_config(page_title='식사 메뉴 해결사')
 st.title('AI 식사 메뉴 해결사 - KING')
 st.caption('점심/저녁등 시점, 날씨, 기분, 단체여부, 예산, MBTI등등 알려주시면 메뉴를 추천해 드립니다.')
 
 # 대화 내용을 기억하는 공간 -> st.session_state 관리
 # 최초
-if "message" not in st.session_state: # 1회만 수행
-    # 내용 추가(기본 가이드) -> 어떤 서비스를 이용할지 최초 가이드 채팅 제공
+if "messages" not in st.session_state: # 1회만 수행
+    # 내용 추가(기본 가이드) -> 어떤 서비스를 이용하지 최초 가이드 채팅 제공
     st.session_state.messages = [
-        # 페르소나는 추후 백엔드에서 고정하여 제공 -> 필요시 고객이 선택
+        # 페르소나는 추후 백엔드에서 고정하여 제공-> 필요시 고객이 선택
         {
             'role':'assistant',
-            'content':'안녕하세요! 오늘 식사는 어떤 메뉴를 추천해 드릴까요? 현재 상황, 기분 등등 알려주세요'
+            'content':'안녕하세요!. 오늘 식사는 어떤 메뉴를 추천해 드릴까요? 현재 상황, 기분 등등 알려주세요'
         }
     ]
 
 # 이전 대화 내용 화면 출력
 for msg in st.session_state.messages:
     # 존재하는 모든 대화 내용 출력
-    with st.chat_message( msg['role']):
-        st.markdown(msg['content'])
+    with st.chat_message( msg['role'] ):
+        st.markdown( msg['content'] )
 
-# 입력창 필요 -> 음성, 텍스트, ...
+# 입력창 필요 -> 음성, `텍스트`, ...
 if prompt := st.chat_input('현재 상황을 자세하게 입력하세요..'):
+    # 전역 상태 창에서 관리하는 저장 공간에 대화 내용 추가
     st.session_state.messages.append({
-        'role' : 'user',
-        'content' : prompt
+        'role'   :'user',
+        'content': prompt
     })
+    
     # 화면에 표기
     with st.chat_message( 'user' ):
         st.markdown( prompt )
@@ -41,7 +42,7 @@ if prompt := st.chat_input('현재 상황을 자세하게 입력하세요..'):
     # 1. 생각 하는 연출 진행
     with st.chat_message( 'assistant' ):
         msg_holder = st.empty()
-        msg_holder.markdown( ' 심각한 고민중 -,.-^..')
+        msg_holder.markdown( '심각한 고민중 ㅡ,.ㅡ^..' )
 
     # 2. 실제 LLM에게 질의 -> FASTAPI 요청 ~/chat
     result = None
@@ -50,11 +51,11 @@ if prompt := st.chat_input('현재 상황을 자세하게 입력하세요..'):
         res = req.post(API_URL, json={"query":prompt})
         # 200 체크
         if res.status_code == 200: # 응답 성공
-            result = res.json()
+            result = res.json().get('response', '일시적 장애입니다.잠시후 다시 시도해주세요')
         else:
-            result = f'서버측 오류{res.status_code}'
+            result = f'서버측 오류 {res.status_code}'
     except Exception as e:
-        print(e)
+        print( e )
         result = f'오류 {e}'
     
     # 화면처리
@@ -62,6 +63,6 @@ if prompt := st.chat_input('현재 상황을 자세하게 입력하세요..'):
 
     # 대화 내용 추가
     st.session_state.messages.append({
-        'role' : 'assistant',
-        'content' : result
+        'role'   :'assistant',
+        'content': result
     })
--- server.py
+++ server.py
@@ -2,15 +2,14 @@
 백엔드 프로그램
 기능
     - ~/chat URL 제공
-    - 클라이언트 채팅입력 => ~/chat 요청 => 프롬프트 구성 => bedrock 호출 => 응답 => 처리 => 프론트
+    - 클라이언트 채팅입력 => ~/chat 요청 => 프럼프트구성 => bedrock 호출 => 응답 => 처리 => 프런트
 '''
-
 # 1. 모듈 가져오기
 from fastapi import FastAPI
 # 입력폼 유효성 검사, 구조 정의
 from pydantic import BaseModel
 # LLM 모듈
-import llm
+from llm import chain
 
 # 2. fastapi 객체 생성
 app = FastAPI(title='식사 메뉴 추천 AI')
@@ -23,4 +22,8 @@ class UserRequest(BaseModel):
 @app.post('/chat')
 async def chat( req:UserRequest ):
     # LLM 호출
-    return f"돈까스 {req.query}"
+    try:
+        response = chain.invoke( {"user_input":req.query} )
+        return {"response":response.content}
+    except Exception as e:
+        return {"response":f"에러 {e}"}
