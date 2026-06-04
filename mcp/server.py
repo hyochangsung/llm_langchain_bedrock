8874fe1b28ffdac18497445042a82ec167a36840
sunghyochang <xx1733@naver.com>
2026-06-04 09:52:44 +0900

    Feat: MCP 서버 기본 구성

---
+++ mcp/server.py
@@ -0,0 +1,19 @@
+'''
+MCP 1.27.2
+외부 도구를 구현한 MCP 서버, FastMCP를 이용하여 간결하게 구성
+'''
+# 1. 모듈 가져오기
+import sys
+import logging
+from datetime import datetime
+from mcp.server.fastmcp import FastMCP
+
+# 2. 로깅 설정
+
+# 3. MCP 서버 설정
+
+# 4. 인메모리
+
+# 5. 툴 구현 (외부에 특정 리소스, s/w, 기타.. ), 편의상 간단한 기능 구성
+
+# 6. 서버 가동
